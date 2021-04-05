96f98b4f8b8ab06bbbdbf6f8ba9dfac8614c3e6d
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-04-05 17:05:52 +0500

    cleaned some things with .gitignore

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -18,8 +18,21 @@ namespace Game{
                 if (res != DialogResult.Yes)
                     eventArgs.Cancel = true;
             };
-            BackgroundImage = ResourcesImages.tempBackground;
-        }
+            //BackgroundImage = ResourcesImages.tempBackground;
+            DoubleBuffered = true;
+            var time = 0;
+            var timer = new Timer();
+            timer.Interval = 5;
+            timer.Tick += (s, arg) => {
+                time++;
+                Invalidate();
+            };
+            Paint += (s, a) => {
+                var r = new Random();
+                a.Graphics.DrawLine(new Pen(Color.Red, 15), 0, 0, r.Next(ClientSize.Width), r.Next(ClientSize.Height));
 
+            };
+            timer.Start();
+        }
     }
 }
