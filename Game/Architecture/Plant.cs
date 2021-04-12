e44ef21b5b172e23843dfb06dd8eba08fd4ba9db
Konstantin <mr.kolmogortsev@gmail.com>
2021-04-12 23:38:50 +0500

    added Plant class

---
+++ Game/Architecture/Plant.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Game {
+    public enum PlantsType {
+        firstBush,
+        secodBush,
+        firstTree,
+        secondTree,
+        fir,
+    }
+    public class Plant : PictureBox {
+        private Dictionary<PlantsType, string> plantsType = new Dictionary<PlantsType, string>() {
+            {PlantsType.firstBush, "firstBush.png"},
+            {PlantsType.secodBush, "secodBush.png"},
+            {PlantsType.firstTree, "firstTree.png"},
+            {PlantsType.secondTree, "secondTree.png"},
+            {PlantsType.fir, "thirdTree.png"},
+        };
+        public Plant(Point location, Size size, PlantsType plantType){
+            Location = location;
+            Size = size;
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            Tag = "plants";
+            Image = new Bitmap(PathToImages + plantsType[plantType]);
+            Visible = true;
+        }
+        protected string PathToImages = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName +
+            "\\Images\\";
+    }
+}
